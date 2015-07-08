8367dcd4c4925719f03a7ad9a5c62c26bc79e7df
Facundo <facundo.gomezalgazan@gmail.com>
2015-07-07 21:08:49 -0300

    Update tests

--- test/nifax/control/controller/AuthenticationTest.java
+++ test/nifax/control/controller/AuthenticationTest.java
@@ -6,9 +6,11 @@
 package nifax.control.controller;
 
 import nifax.control.exception.InitializeSessionException;
+import nifax.control.exception.InvalidCredentialsException;
 import nifax.control.model.Employee;
 
 import nifax.control.model.UserEmployee;
+import nifax.control.model.modeler.ModelOperation;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -18,28 +20,19 @@ import static org.junit.Assert.*;
 import org.junit.Rule;
 import org.junit.rules.ExpectedException;
 
+
 /**
  *
  * @author faka
  */
-public class AuthenticationTest {
-    private UserEmployee usr;
-    private Authentication instance;
-    
+public class AuthenticationTest {    
     public AuthenticationTest() {
     }
     
     @BeforeClass
     public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-        usr = new UserEmployee(
+        ModelOperation op = ModelOperation.getInstance();
+        UserEmployee usr = new UserEmployee(
             "f4ka", 
             "1234", 
             new Employee(
@@ -47,40 +40,58 @@ public class AuthenticationTest {
                 "Gomez Algazan", 
                 "+5493816534690"
             )
-        );                
+        );
+        op.Insert(usr);        
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+
+    @Before
+    public void setUp() {                       
     }
     
     @After
-    public void tearDown() throws InitializeSessionException {
-        instance.closeSession();
+    public void tearDown() {
+        
     }
     @Rule
      public ExpectedException thrown= ExpectedException.none();
     /**
      * Test of initSession method, of class Authentication.
+     * @throws nifax.control.exception.InvalidCredentialsException
      */
     @Test
-    public void testInitSession() {
-        System.out.println("initSession works");
-        instance = Authentication.getInstance();
-        instance.initSession(usr);
+    public void testLogIn() throws InvalidCredentialsException {
+        System.out.println("LogIn works");
+        Authentication instance = Authentication.getInstance();
+        assertTrue(instance.LogIn("f4ka", "1234"));
     }
-
+    
     @Test
-    public void testInitSessionInvalid() throws IllegalArgumentException{
-        thrown.expect(IllegalArgumentException.class);        
-        System.out.println("initSession fails");
-        instance = Authentication.getInstance();
-        instance.initSession(null);
+    public void testLogInInvalid() throws InvalidCredentialsException{
+        System.out.println("LogIn fails");
+        Authentication instance = Authentication.getInstance();
+        assertFalse(instance.LogIn("f4ka", "12345"));
+    }
+    
+    @Test
+    public void testLogOut() throws InvalidCredentialsException, InitializeSessionException {
+        System.out.println("LogOut works");
+        Authentication instance = Authentication.getInstance();
+        instance.LogIn("f4ka", "1234");        
+        assertTrue(instance.LogOut(instance.getSession()));
+        assertFalse(instance.getSession().isOpen());
     }
     
     /**
      * Test of getInstance method, of class Authentication.
      */
     @Test
-    public void testGetInstance() {
+    public void testGetInstance(){
         System.out.println("getInstance works");
-        instance = Authentication.getInstance();
+        Authentication instance = Authentication.getInstance();
         assertNotNull(instance);
     }
 
@@ -88,11 +99,12 @@ public class AuthenticationTest {
      * Test of getSession method, of class Authentication.
      * @throws nifax.control.exception.InitializeSessionException
      */
-    @Test
+   @Test
     public void testGetSession() throws InitializeSessionException {
         thrown.expect(InitializeSessionException.class);
         System.out.println("getSession fails");
-        instance = Authentication.getInstance();
+        Authentication instance = Authentication.getInstance();
+        instance.NullifySession();
         instance.getSession();
     }
     
