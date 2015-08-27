99003e70ebe8d487795b7792649f1ab64379be86
nicolasbloj@gmail.com <nicolasbloj@gmail.com>
2015-08-27 00:13:06 -0300

    Refactoring Operation Sale

---
+++ src/nifax/control/view/event/txf/Txf_operationtListener.java
@@ -0,0 +1,33 @@
+package nifax.control.view.event.txf;
+
+import com.sun.glass.events.KeyEvent;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import nifax.control.controller.Operation;
+import nifax.control.util.Message;
+
+/**
+ *
+ * @author NB
+ */
+public class Txf_operationtListener extends Txf_Listener {
+
+    private final JPanel panel;
+    private final int action;
+
+    public Txf_operationtListener(JPanel panel, int action) {
+        this.panel = panel;
+        this.action = action;
+
+    }
+
+    @Override
+    public void keyPressed(java.awt.event.KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_ENTER) {
+            if (!Operation.getInstance().operate(panel,"", action)) {
+                JOptionPane.showMessageDialog(null, Message.FailuredOperation,
+                    Message.FailuredOperationTitle, JOptionPane.ERROR_MESSAGE);
+            }
+        }
+    }
+}
